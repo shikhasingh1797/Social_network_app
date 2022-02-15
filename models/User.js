1a5b15879781ab7dcea96b91e58a819126201f68
myusername <signups@fabiopacifici.com>
2022-02-16 02:15:40 +0530

    first commit

--- models/User.js
+++ models/User.js
@@ -2,12 +2,15 @@ const mongoose=require("mongoose");
 const UserSchema=new mongoose.Schema({
     name:{
         type:String,
-        required=true
+        required:true
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        lowercase:true,
+        unique:true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        required: 'Email address is required'
     },
     password:{
         type:String,
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const app = express();
-
+const express=require("express")
 const router=express.Router();
-const { check, validationResult } = require('express-validator');
 
-app.use(express.json());
-
- 
-
-router.post('/',
-    check('name','Name is required')
-    .not()
-    .isEmpty(),
-    check('email','Please include a valid email').isEmail(),
-    check('password','Please enter a password with six chacrcter or more than sex'
-    ).isLength()({min:6})
-,(req,res)=>{
-    console.log(req.body)
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array() });
-    }
-    res.send("User Route")
-})
+router.get('/',(req,res)=>res.send("User Route"))
 
 module.exports=router;
+
+
+
+// const express = require("express");
+// const app = express();
+
+// const dataModel = require('./../../models/User.js');
+
+// app.post("/", async (request, response) => {
+//   const data = new dataModel(request.body)
+//   try {
+//     await data.save();
+//     response.send(data);
+//   } catch (error) {    
+//     response.status(400).send(error.message);
+//   }
+// });
+// module.exports = app;
